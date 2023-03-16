ffdb394cdebfe331f8f5a3dabe82f286989fa99a
Taras Sakhno <tsakhno@edu.cdv.pl>
2023-03-16 15:27:30 +0100

    Funny comment

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -7,7 +7,7 @@ public class NewBehaviourScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //hahahahah funny comment
     }
 
     // Update is called once per frame
